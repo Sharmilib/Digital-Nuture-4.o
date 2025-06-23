7aafc88e9824c71e43e15b0868b418d96f5dbe39
Sharmilib <2022peccb174@gmail.com>
2025-06-23 21:14:44 +0530

    Create Main.java

---
+++ Week1/Exercise1/Main.java
@@ -0,0 +1,21 @@
+package cognizant;
+//Main.java
+public class Main {
+ public static void main(String[] args) {
+     // Getting first instance
+     Logger logger1 = Logger.getInstance();
+     logger1.log("This is the first log message.");
+
+     // Getting second instance
+     Logger logger2 = Logger.getInstance();
+     logger2.log("This is the second log message.");
+
+     // Verifying both references point to the same object
+     if (logger1 == logger2) {
+         System.out.println("Both logger1 and logger2 are the same instance.");
+     } else {
+         System.out.println("Different instances exist.");
+     }
+ }
+}
+
